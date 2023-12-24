24034c009aa0c05381f508999d7294fadb4fd9c3
avigail Buxbaum <avigb041@gmail.com>
2023-12-24 01:10:11 +0100

    styls of inpot

--- src/cmps/EmailFilter.jsx
+++ src/cmps/EmailFilter.jsx
@@ -1,17 +1,19 @@
+import { IoMdSearch } from "react-icons/io";
 
-export function EmailFilter({onSetFilter}) {
-
+export function EmailFilter({ onSetFilter }) {
   function handleChange(ev) {
     const field = ev.target.name
     const value = ev.target.value
-    onSetFilter({[field]: value})
+    onSetFilter({ [field]: value })
   }
 
   return (
     <section className="email-filter">
-      <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_rtl_r5.png"/>
-      <form></form>
-      <input onChange={handleChange} name="txt" type="text" />
+      <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_rtl_r5.png" />
+      <form>
+      <IoMdSearch className="search"/>
+        <input onChange={handleChange} name="txt" type="text" placeholder="Search mail"/>
+      </form>
     </section>
   )
 }
